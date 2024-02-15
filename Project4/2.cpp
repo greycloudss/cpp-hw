1cdf5899e09f3f5c326b4a9f21ac1ff5e81d9641
Dovydas Levinas <dovydas.levinas@mif.stud.vu.lt>
2024-02-15 16:19:19 +0200

    02/15

--- Project4/2.cpp
+++ Project4/2.cpp
@@ -3,143 +3,138 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <vector>
-#define max 1000
+#include <string>
 
+class Window {
+private:
+	float x, y;
 
-void bubbleSort(std::vector<int> arr, int n) {
-	int tmp;
-	bool status = false;
-	for (int i = 0; i < n; i++) {
-		status = false;
-
-
-		for (int ii = arr.begin(); ii < n - i - 1; ii++) {
-			if (arr[ii] > arr[ii + 1]) {
-				std::swap(arr[ii], arr[ii + 1]);
-				status = true;
-			}
-		}
-
-		if (status == false)
-			break;
-		if (arr[i] == 0)
-			break;
+public:
+	void setX(float val) {
+		this->x = val;
+	}
+	void setY(float val) {
+		this->y = val;
 	}
 
-
-}
-
-void printArray(std::vector<int> arr, int n) {
-	for (int i = 0; i < n; i++) {
-		if (arr[i] == 0)
-			return;
-
-		std::cout << arr[i] << " ";
+	float retX() {
+		return this->x;
 	}
-}
+	float retY() {
+		return this->y;
+	}
+};
 
-int main(void) {
-	int tmp;
-	std::vector<int> arr;
-	int n = 0;
-	std::cout << "Hello world!";
-	Sleep(3000);
-	do {
-		system("cls");
-		//std::cout << "Enter any integer you wish. the maximum is 1000 integers, however if you enter 0 the program stops" << std::endl;
-		std::cout << "Enter an integer: ";
-		std::cin >> tmp;
-		if (tmp != 0) {
-			arr.push_back(tmp);
-			n++;
-		}
-		else {
-			system("cls");
-			printArray(arr, n); // unsorted
-			bubbleSort(arr, n);
-			puts("\n\n");
-			printArray(arr, n); //sorted
-			break;
-		}
+class Doors {
+private:
+	float x, y;
 
-	}while (1);
-
-	return 0;
-}
-
-/*
-* 
-  --------------------
- |  1 2 ir 3 uzduotys |
-  --------------------
-* 
-* 
-* 
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-#include <Windows.h>
-#include <vector>
-#define max 1000
-
-
-void bubbleSort(int arr[], int n) {
-	int tmp;
-	bool status = false;
-	for (int i = 0; i < n; i++) {
-		status = false;
-		
-
-		for (int ii = 0; ii < n - i - 1; ii++) {
-			if (arr[ii] > arr[ii + 1]) {
-				std::swap(arr[ii], arr[ii + 1]);
-				status = true;
-			}
-		}
-
-		if (status == false)
-			break;
-		if (arr[i] == 0)
-			break;
+public:
+	void setX(float val) {
+		this->x = val;
+	}
+	void setY(float val) {
+		this->y = val;
 	}
 
-
-}
-
-void printArray(int arr[], int n) {
-	for (int i = 0; i < n; i++) {
-		if (arr[i] == 0)
-			return;
-
-		std::cout << arr[i] << " ";
+	float retX() {
+		return this->x;
 	}
-}
+	float retY() {
+		return this->y;
+	}
+};
 
-int main(void) {
-	int tmp;
-	int arr[max] = { 0 };
-	int n = 0;
-	std::cout << "Hello world!";
-	Sleep(3000);
-	for (int i = 0; i < max; i++) {
-		system("cls");
-		//std::cout << "Enter any integer you wish. the maximum is 1000 integers, however if you enter 0 the program stops" << std::endl;
-		std::cout << "Enter an integer: ";
-		std::cin >> tmp;
-		if (tmp != 0) {
-			arr[i] = tmp;
-			n++;
-		}
-		else {
-			system("cls");
-			printArray(arr, n); // unsorted
-			bubbleSort(arr, n);
-			puts("\n\n");
-			printArray(arr, n); //sorted
-			break;
-		}
+class Room {
+private:
+	float x, y, z;
+	std::vector<Doors> door;
+	std::vector<Window> window;
+public:
+	void addWindow(Window arg) {
+		window.push_back(arg);
 	}
 
-	return 0;
+	void addDoors(Doors arg) {
+		door.push_back(arg);
+	}
+	float getC(char coord) {
+		coord = tolower(coord);
+		if (coord == 'x')
+			return this->x;
+		if (coord == 'y')
+			return this->y;
+		if (coord == 'z')
+			return this->z;
+		return 0;
+	}
+
+	void setC(char coord, float val) {
+		coord = tolower(coord);
+		if (coord == 'x')
+			this->x = val;
+		if (coord == 'y')
+			this->y = val;
+		if (coord == 'z')
+			this->z = val;
+	}
+
+	float wallArea() {
+		float wall_area = 4 * (this->x * this->y);
+		float remArea = 0;
+
+		for (auto i = (this->door).begin(); i != (this->door).end(); ++i)
+			remArea += i->retX() * i->retY();
+
+		for (auto i = (this->window).begin(); i != (this->window).end(); ++i)
+			remArea += i->retX() * i->retY();
+
+		return wall_area - remArea;
+	}
+
+	float gridFloor() {
+		float fl_perim = this->x + this->z;
+
+		for (auto& d : door)
+			fl_perim -= d.retX();
+
+		return fl_perim;
+	}
+
+};
+
+
+void main() {
+	Window A[2];
+	Doors B[2];
+	Room AB;
+
+	//windows
+	A[0].setX(10.5);
+	A[0].setY(5.6);
+
+	A[1].setX(5.5);
+	A[1].setY(9.5);
+
+	AB.addWindow(A[0]);
+	AB.addWindow(A[1]);
+
+
+	//doors
+	B[0].setX(10.5);
+	B[0].setY(5.6);
+
+	B[1].setX(5.5);
+	B[1].setY(9.5);
+
+	AB.addDoors(B[0]);
+	AB.addDoors(B[1]);
+
+	//room
+	AB.setC('x', (float)50.2);
+	AB.setC('y', (float)20.5);
+	AB.setC('z', (float)20.3);
+
+	std::cout << "Grid: " << AB.gridFloor() << "; Wall Area: " << AB.wallArea() << std::endl;
+
 }
-*/
